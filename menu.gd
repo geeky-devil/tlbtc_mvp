9700cefdbcc4472b711d2f2ccb1b138e0d398bcc
Omkar Parekar <omkarparekar@gmail.com>
2024-11-13 12:47:35 +0530

    Added audio playback

--- menu.gd
+++ menu.gd
@@ -2,16 +2,17 @@ extends Control
 @onready var micButton=$MicButton
 @onready var animPlayer=$AnimationPlayer
 @onready var menuMusic=$AudioStreamPlayer2D
-@onready var volume_bar=$VolumeBar
-@onready var volume_txt=$VolumeText
+@onready var volume_bar=$MicContainer/VolumeBar
+@onready var volume_txt=$MicContainer/VolumeText
+@onready var mic_container=$MicContainer
 @onready var audio_out=$micOutput
 var reverse=false
 var record_bus_index:int
 var record_effect:AudioEffectRecord
 var recording:AudioStream
 func _ready():
-	volume_bar.self_modulate.a=0
-	volume_txt.self_modulate.a=0
+	mic_container.modulate.a=0
+	mic_container.process_mode=Node.PROCESS_MODE_DISABLED
 	print('ready')
 	animPlayer.play('title')
 	menuMusic.play()
@@ -26,26 +27,22 @@ func _process(_delta):
 	volume_txt.text='%s db'%round(sample)
 
 func _on_animplayer_animation_finished(anim_name):
-	#print(anim_name,'finished')
-	if animPlayer.current_animation=='micTest':
-		print('something')
-	pass
+	print(anim_name,reverse)
+	if anim_name=="micTest":
+		if reverse:
+			print('AUdio enabled')
+			mic_container.process_mode=Node.PROCESS_MODE_INHERIT
+			
 
 func _on_mic_button_pressed():
-	if animPlayer.is_playing() and animPlayer.current_animation=='title':return
-	print(animPlayer.current_animation,':',animPlayer.is_playing())
-	if record_effect.is_recording_active():
-		stop_recording()
-		print('Stoping')
-	else:
-		print('Recording')
-		start_recording()
+	#if animPlayer.is_playing() and animPlayer.current_animation=='title':return
+	if animPlayer.is_playing():return
 	if reverse:
-		#return
-		#print('reverse')
 		animPlayer.play_backwards('micTest')
 		menuMusic.play()
+		mic_container.process_mode=Node.PROCESS_MODE_DISABLED
 		reverse=!reverse
+		print('AUdio disabled')
 		return
 	animPlayer.play("micTest")
 	menuMusic.stop()
@@ -55,6 +52,20 @@ func start_recording():
 	record_effect.set_recording_active(true)
 func stop_recording():
 	record_effect.set_recording_active(false)
-	#recording=record_effect.get_recording()
-	#audio_out.stream=recording
+	recording=record_effect.get_recording()
+	audio_out.stream=recording
 	#audio_out.play()
+
+
+func _on_record_pressed():
+	print('record pressed')
+	if record_effect.is_recording_active():
+		stop_recording()
+		%Record.text='Record'
+	else:
+		%Record.text='Stop'
+		start_recording()
+
+
+func _on_play_pressed():
+	audio_out.play()
